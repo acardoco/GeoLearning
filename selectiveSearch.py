de876875be37bdf3ce9b2bb2962f5655d4a2b744
acardoco <andres.cardosoc12@gmail.com>
2018-02-14 18:52:31 +0100

    selección de zonas + clasificacion
    falta enseñar outputs y elegir mejor

--- selectiveSearch.py
+++ selectiveSearch.py
@@ -6,127 +6,114 @@ from __future__ import (
 import skimage.data
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+from keras.preprocessing.image import img_to_array, load_img 
+from keras.models import Sequential  
+from keras.layers import Dropout, Flatten, Dense 
+from keras import applications 
 import selectivesearch
 import numpy as np
-import cv2
 
 from PIL import Image
 
-
-ciudadespath = 'pruebas/ciudades/ciudad2.jpg'
+#general parametters
+ciudadespath = 'pruebas/ciudades/ciudad6.jpg'
+top_model_weights_path = 'bottleneck_fc_model.h5' 
+size = 40, 40
 
 def load_image( infilename ) :
     img = Image.open( infilename )
     img.load()
     data = np.asarray( img, dtype="uint8" )
     return data
-'''
+
+    '''#-----ejemplo BORRAR LUEGO
+    im = Image.open(ciudadespath)
+    crop_rectangle = (66, 0, 31+66, 16)
+    cropped_im = im.crop(crop_rectangle)
+    cropped_im.show()
+    #----'''
+
 #--------------------------------------------------------------------
 #--------------------------------------------------------------------
 #funcion para predecir varias imagenes a partir de un directorio dado
 #--------------------------------------------------------------------
 #--------------------------------------------------------------------
-def predictMultiple(directory, imag_number, imag_format, imag_dim):
+def predictMultiple( x, y, w, h):
     # load the class_indices saved in the earlier step
     class_dictionary = np.load('class_indices.npy').item()
 
     num_classes = len(class_dictionary)
 
-    for i in range(0,imag_number):
+    #print("[INFO] loading and preprocessing image...")
+    image = load_img(ciudadespath)
+    crop_rectangle = (x, y, w + x , h + y)
+    image = image.crop(crop_rectangle)
+    image = image.resize(size)
+    image = img_to_array(image)
 
-        # add the path to your test image below
-        image_path = 'pathimagen' + i + '.' + imag_format
+    # important! otherwise the predictions will be '0'
+    image = image / 255
 
-        orig = cv2.imread(image_path)
+    image = np.expand_dims(image, axis=0)
 
-        print("[INFO] loading and preprocessing image...")
-        image = load_img(image_path, target_size=(imag_dim, imag_dim))
-        image = img_to_array(image)
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
 
-        # important! otherwise the predictions will be '0'
-        image = image / 255
+    # get the bottleneck prediction from the pre-trained VGG16 model
+    bottleneck_prediction = model.predict(image)
 
-        image = np.expand_dims(image, axis=0)
+    # build top model
+    model = Sequential()
+    model.add(Flatten(input_shape=bottleneck_prediction.shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='sigmoid'))
 
-        # build the VGG16 network
-        model = applications.VGG16(include_top=False, weights='imagenet')
+    model.load_weights(top_model_weights_path)
 
-        # get the bottleneck prediction from the pre-trained VGG16 model
-        bottleneck_prediction = model.predict(image)
+    # use the bottleneck prediction on the top model to get the final
+    # classification
+    class_predicted = model.predict_classes(bottleneck_prediction)
 
-        # build top model
-        model = Sequential()
-        model.add(Flatten(input_shape=bottleneck_prediction.shape[1:]))
-        model.add(Dense(256, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(num_classes, activation='sigmoid'))
+    probabilities = model.predict_proba(bottleneck_prediction)
 
-        model.load_weights(top_model_weights_path)
+    inID = class_predicted[0]
 
-        # use the bottleneck prediction on the top model to get the final
-        # classification
-        class_predicted = model.predict_classes(bottleneck_prediction)
-
-        probabilities = model.predict_proba(bottleneck_prediction)
+    print ("inID",class_predicted[0],"probabilities",probabilities[0])
 
 
-def asigProb():
-    # loading test image
-    img = load_image(ciudadespath)
-
-    # perform selective search
-    img_lbl, regions = selectivesearch.selective_search(
-        img, scale=100, sigma=0.9, min_size=10)
-
-    candidates = set()    
-
-    #a cada region se le aplica la red entrenada
-    for r in regions:
-        # excluding same rectangle (with different segments)
-        if r['rect'] in candidates:
-            continue
-        # excluding regions smaller than 2000 pixels
-        if r['size'] < 2000:
-            continue
-        # distorted rects
-        x, y, w, h = r['rect']
-        if w / h > 1.2 or h / w > 1.2:
-            continue
-        candidates.add(r['rect'])
-'''
 def main():
 
    # loading astronaut image
     img = load_image(ciudadespath)
+    im_clasiffier = Image.open(ciudadespath)
 
     # perform selective search
     img_lbl, regions = selectivesearch.selective_search(
-        img, scale=100, sigma=0.9, min_size=10)
+        img, scale=50, sigma=0.9, min_size=10)
 
-    #-----ejemplo BORRAR LUEGO
-    im = Image.open(ciudadespath)
-    crop_rectangle = (20, 20, 58, 69)
-    cropped_im = im.crop(crop_rectangle)
-    cropped_im.show()
-    #----
+    candidates = set() 
 
-    candidates = set()
     for r in regions:
         # excluding same rectangle (with different segments)
         if r['rect'] in candidates:
             continue
         # excluding regions smaller than 2000 pixels
-        if r['size'] < 2000:
+        if r['size'] < 200:
             continue
         # distorted rects
         x, y, w, h = r['rect']
         if w / h > 1.2 or h / w > 1.2:
             continue
 
+        #calling the clasiffier
+        predictMultiple(x, y, w, h)
+
 
         candidates.add(r['rect'])
 
-    '''regions[:10]
+    print("Regiones seleccionadas:",candidates.__len__())
+    
     # draw rectangles on the original image
     fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(6, 6))
     ax.imshow(img)
@@ -136,7 +123,7 @@ def main():
             (x, y), w, h, fill=False, edgecolor='red', linewidth=1)
         ax.add_patch(rect)
 
-    plt.show()'''
+    plt.show()
 
 if __name__ == "__main__":
     main()
